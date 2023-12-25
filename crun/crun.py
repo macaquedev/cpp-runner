003757e7d5ba4102628693591ce82b3529444807
Alex Pylypenko <macaquedev@gmail.com>
2023-12-25 14:03:13 +0000

    Updated templates to support printing big data structures

--- crun/crun.py
+++ crun/crun.py
@@ -5,13 +5,12 @@ home = os.path.expanduser("~")
 template = """#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define nL "\\n"
 #define pb push_back
 #define mk make_pair
 #define pii pair<int, int>
-#define a first
-#define b second
 #define vi vector<int>
+#define vpii vector<pii>
+#define vs vector<string>
 #define all(x) (x).begin(), (x).end()
 #define umap unordered_map
 #define uset unordered_set
@@ -20,13 +19,149 @@ using namespace std;
 #define imin INT_MIN
 #define exp 1e9
 #define sz(x) (int((x).size()))
+#define elif else if
+
+
+template<typename T, typename Y>
+auto operator<<(std::ostream& os, const std::pair<T,Y>& p) -> std::ostream&
+{
+    os << '(';
+    if (typeid(T) == typeid(string)) {
+        os << '"' << p.first << '"';
+    }
+    else {
+        os << p.first;
+    }
+    os << ", ";
+    if (typeid(Y) == typeid(string)) {
+        os << '"' << p.second << '"';
+    }
+    else {
+        os << p.second;
+    }
+    os << ')';
+    return os;
+}
+
+template<typename T>
+auto operator<<(std::ostream& os, const std::vector<T>& v) -> std::ostream&
+{
+    os << "[";
+    if (v.size() == 0) {
+        os << "]";
+        return os;
+    }
+    if (typeid(T) == typeid(string)) {
+        os << '"' << v[0] << '"';
+    }
+    else {
+        os << v[0];
+    }
+    for (int i=1; i<v.size(); i++) {
+        if (typeid(T) == typeid(string)) {
+            os << ", \"" << v[i] << '"';        
+        }
+        else {
+            os << ", " << v[i];
+        }
+    }
+    os << "]";
+    return os;
+}
+
+template<typename T>
+auto operator<<(std::ostream& os, const std::set<T>& s) -> std::ostream&
+{
+    if (s.size() == 0) {
+        os << "{}";
+        return os;
+    }
+    os << "{";
+    
+    int i=0;
+
+    for (auto value : s) {
+        if (i++ != 0) os << ", ";
+        if (typeid(T) == typeid(string)) os << '"' << value << '"';
+        else os << value;
+    }
+    os << "}";
+    return os; 
+}
+
+template<typename T>
+auto operator<<(std::ostream& os, const std::unordered_set<T>& s) -> std::ostream&
+{
+    if (s.size() == 0) {
+        os << "{}";
+        return os;
+    }
+    os << "{";
+    
+    int i=0;
+
+    for (auto value : s) {
+        if (i++ != 0) os << ", ";
+        if (typeid(T) == typeid(string)) os << '"' << value << '"';
+        else os << value;
+    }
+    os << "}";
+    return os; 
+}
+
+template<typename T, typename Y>
+auto operator<<(std::ostream& os, const std::map<T, Y>& m) -> std::ostream&
+{
+    if (m.size() == 0) {
+        os << "{}";
+        return os;
+    }
+    os << "{";
+    
+    int i=0;
+
+    for (auto [key, value] : m) {
+        if (i++ != 0) os << ", ";
+        if (typeid(T) == typeid(string)) os << '"' << key << '"';
+        else os << key;
+        os << ": ";
+        if (typeid(Y) == typeid(string)) os << '"' << value << '"';
+        else os << value;
+    }
+    os << "}";
+    return os; 
+}  
+
+template<typename T, typename Y> 
+auto operator<<(std::ostream& os, const std::unordered_map<T, Y>&  m) -> std::ostream&
+{
+    if (m.size() == 0) {
+        os << "{}";
+        return os;
+    }
+    os << "{";
+    
+    int i=0;
+
+    for (auto [key, value] : m) {
+        if (i++ != 0) os << ", ";
+        if (typeid(T) == typeid(string)) os << '"' << key << '"';
+        else os << key;
+        os << ": ";
+        if (typeid(Y) == typeid(string)) os << '"' << value << '"';
+        else os << value;
+    }
+    os << "}";
+    return os; 
+}
+
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int ttt; cin >> ttt;
     while(ttt--) {
-        cout << ttt << endl;
+        
     }
     return 0;
 }"""
@@ -39,13 +174,12 @@ TASK: %s
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define nL "\\n"
 #define pb push_back
 #define mk make_pair
 #define pii pair<int, int>
-#define a first
-#define b second
 #define vi vector<int>
+#define vpii vector<pii>
+#define vs vector<string>
 #define all(x) (x).begin(), (x).end()
 #define umap unordered_map
 #define uset unordered_set
@@ -54,10 +188,148 @@ using namespace std;
 #define imin INT_MIN
 #define exp 1e9
 #define sz(x) (int((x).size()))
+#define elif else if
+
+
+template<typename T, typename Y>
+auto operator<<(std::ostream& os, const std::pair<T,Y>& p) -> std::ostream&
+{
+    os << '(';
+    if (typeid(T) == typeid(string)) {
+        os << '"' << p.first << '"';
+    }
+    else {
+        os << p.first;
+    }
+    os << ", ";
+    if (typeid(Y) == typeid(string)) {
+        os << '"' << p.second << '"';
+    }
+    else {
+        os << p.second;
+    }
+    os << ')';
+    return os;
+}
+
+template<typename T>
+auto operator<<(std::ostream& os, const std::vector<T>& v) -> std::ostream&
+{
+    os << "[";
+    if (v.size() == 0) {
+        os << "]";
+        return os;
+    }
+    if (typeid(T) == typeid(string)) {
+        os << '"' << v[0] << '"';
+    }
+    else {
+        os << v[0];
+    }
+    for (int i=1; i<v.size(); i++) {
+        if (typeid(T) == typeid(string)) {
+            os << ", \"" << v[i] << '"';        
+        }
+        else {
+            os << ", " << v[i];
+        }
+    }
+    os << "]";
+    return os;
+}
+
+template<typename T>
+auto operator<<(std::ostream& os, const std::set<T>& s) -> std::ostream&
+{
+    if (s.size() == 0) {
+        os << "{}";
+        return os;
+    }
+    os << "{";
+    
+    int i=0;
+
+    for (auto value : s) {
+        if (i++ != 0) os << ", ";
+        if (typeid(T) == typeid(string)) os << '"' << value << '"';
+        else os << value;
+    }
+    os << "}";
+    return os; 
+}
+
+template<typename T>
+auto operator<<(std::ostream& os, const std::unordered_set<T>& s) -> std::ostream&
+{
+    if (s.size() == 0) {
+        os << "{}";
+        return os;
+    }
+    os << "{";
+    
+    int i=0;
+
+    for (auto value : s) {
+        if (i++ != 0) os << ", ";
+        if (typeid(T) == typeid(string)) os << '"' << value << '"';
+        else os << value;
+    }
+    os << "}";
+    return os; 
+}
+
+template<typename T, typename Y>
+auto operator<<(std::ostream& os, const std::map<T, Y>& m) -> std::ostream&
+{
+    if (m.size() == 0) {
+        os << "{}";
+        return os;
+    }
+    os << "{";
+    
+    int i=0;
+
+    for (auto [key, value] : m) {
+        if (i++ != 0) os << ", ";
+        if (typeid(T) == typeid(string)) os << '"' << key << '"';
+        else os << key;
+        os << ": ";
+        if (typeid(Y) == typeid(string)) os << '"' << value << '"';
+        else os << value;
+    }
+    os << "}";
+    return os; 
+}  
+
+template<typename T, typename Y> 
+auto operator<<(std::ostream& os, const std::unordered_map<T, Y>&  m) -> std::ostream&
+{
+    if (m.size() == 0) {
+        os << "{}";
+        return os;
+    }
+    os << "{";
+    
+    int i=0;
+
+    for (auto [key, value] : m) {
+        if (i++ != 0) os << ", ";
+        if (typeid(T) == typeid(string)) os << '"' << key << '"';
+        else os << key;
+        os << ": ";
+        if (typeid(Y) == typeid(string)) os << '"' << value << '"';
+        else os << value;
+    }
+    os << "}";
+    return os; 
+}
 int32_t main()
 {
-    ifstream fin("%s.in");
-    ofstream fout("%s.out");
+    ifstream cin("%s.in");
+    ofstream cout("%s.out");
+    
+    
+    
     return 0;
 }"""
 
@@ -82,9 +354,22 @@ def cli():
             else:
                 f.write(template)
     else:
-        os.system(f"g++ {args.filename}.cpp")
+        with open(f"{args.filename}.cpp") as f:
+            data = f.readlines()
+
+        with open("a.cpp", "w+") as f:
+            for i in range(len(data)):
+                data[i] = data[i].replace(f'ifstream cin("{args.filename}.in");', '')
+                data[i] = data[i].replace(f'ofstream cout("{args.filename}.out");', '')
+                data[i] = data[i].replace('ios_base::sync_with_stdio(false);', '')
+                data[i] = data[i].replace('cin.tie(NULL);', '')
+                print(data[i])
+            f.writelines(data)
+
+        os.system(f"g++ a.cpp")
         os.system(f"./a.out")
         os.system(f"rm -rf ./a.out")
+        os.system(f"rm -rf ./a.cpp")
 
 
 if __name__ == "__main__":
